ed4c2bffc6f32e5690bd21688c58d455173341aa
Abdullah Rasool <abdullahrasool849@gmail.com>
2024-06-26 01:54:28 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Example currency rates relative to USD
+const currency = {
+    USD: 1, // Base Currency
+    EUR: 0.93,
+    GBP: 0.79,
+    INR: 83.42,
+    PKR: 278.50,
+    JPY: 0.0063,
+    CAD: 0.73,
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "fromCurrency",
+        message: "Enter from Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "JPY", "CAD"],
+    },
+    {
+        name: "toCurrency",
+        message: "Enter to Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "JPY", "CAD"],
+    },
+    {
+        name: "amountToConvert",
+        message: "Enter your Amount:",
+        type: "number",
+    }
+]);
+let fromRate = currency[userAnswer.fromCurrency];
+let toRate = currency[userAnswer.toCurrency];
+let amountToConvert = userAnswer.amountToConvert;
+// Formula for conversion
+let baseAmount = amountToConvert / fromRate; // Convert to USD (base currency)
+let convertedAmount = baseAmount * toRate; // Convert from USD to target currency
+console.log("Your Coverted Anount is:", Math.round(convertedAmount));
+console.log("\n Thank you! \n");
